78682a855e568c619540c3a71e336890f237955a
imagidan-ets <daniel.danaie.1@ens.etsmtl.ca>
2024-08-30 07:29:35 -0400

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 const ClientSideRouting = {
   name: "dynamic-router",
   configureServer(server: any) {
-    server.middlewares.use((req: any, res: any, next: any) => {
+    server.middlewares.use((req: any, _res: any, next: any) => {
       if (req.url.search(/^\/@\d+/) !== -1 || ['/team', '/prototype', '/competition'].includes(req.url)) {
         req.url = "/";
       }
